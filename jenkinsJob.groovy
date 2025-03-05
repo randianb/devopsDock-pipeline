d14dc792926e85bc575961ef67673929d1339d66
Anouar Harrou <95416938+anouarharrou@users.noreply.github.com>
2025-03-05 11:35:45 +0000

    Update jenkinsJob.groovy

--- jenkinsJob.groovy
+++ jenkinsJob.groovy
@@ -1,32 +1,3 @@
-this's the error i got : 
-+ curl -s --user '****:****' https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/lastSuccessfulBuild/buildNumber
-[Pipeline] echo
-Latest Successful Build Number: 32
-[Pipeline] sh
-+ curl -s --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/32/api/json?tree=actions%5Bparameters%5B*%5D%5D'
-[Pipeline] sh
-+ curl -s -o /dev/null -w '%{http_code}' --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/32/api/json?tree=actions%5Bparameters%5B*%5D%5D'
-[Pipeline] echo
-Curl Status: 200
-[Pipeline] echo
-Build Info JSON: {"_class":"org.jenkinsci.plugins.workflow.job.WorkflowRun","actions":[{"_class":"hudson.model.ParametersAction","parameters":[{"_class":"hudson.model.StringParameterValue","name":"SERVICE","value":"unibank-branch-onboarding"},{"_class":"hudson.model.StringParameterValue","name":"ENV","value":"uat"},{"_class":"hudson.model.BooleanParameterValue","name":"REBOOT","value":true}]},{"_class":"hudson.model.CauseAction"},{"_class":"jenkins.metrics.impl.TimeInQueueAction"},{},{"_class":"org.jenkinsci.plugins.workflow.libs.LibrariesAction"},{},{"_class":"hudson.plugins.git.util.BuildData"},{},{"_class":"org.jenkinsci.plugins.workflow.cps.EnvActionImpl"},{},{"_class":"hudson.plugins.git.util.BuildData"},{"_class":"org.jenkinsci.plugins.workflow.cps.view.InterpolatedSecretsAction"},{},{},{},{},{"_class":"org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction"},{"_class":"org.jenkinsci.plugins.pipeline.modeldefinition.actions.RestartDeclarativePipelineAction"},{},{"_class":"org.jenkinsci.plugins.workflow.job.views.FlowGraphAction"},{},{},{}]}
-[Pipeline] readJSON
-[Pipeline] echo
-Parameters for New Build: SERVICE=unibank-branch-onboarding&ENV=uat&REBOOT=true
-[Pipeline] sh
-/var/jenkins_home/jobs/DJD/jobs/Build-Archived/jobs/JenkinsCron/workspace@tmp/durable-0b8f8032/script.sh.copy: line 2: syntax error near unexpected token `('
-[Pipeline] }
-[Pipeline] // script
-[Pipeline] }
-[Pipeline] // withCredentials
-[Pipeline] }
-[Pipeline] // stage
-[Pipeline] }
-[Pipeline] // node
-[Pipeline] End of Pipeline
-ERROR: script returned exit code 2
-                                                                                                                                                            
-here's the pipeline : 
 def jenkinsUrl = "https://cdp-jenkins-paas-xsf.fr.world.socgen"
 def jobPath = "job/DJD/job/CD-Deploy/job/Reboot"
 
@@ -44,8 +15,9 @@ pipeline {
                         // Get the last successful build number
                         def buildNumber = sh(
                             script: """
-                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
-                            '${jenkinsUrl}/${jobPath}/lastSuccessfulBuild/buildNumber'""",
+                            curl -s --user ${JENKINS_USER}:${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/${jobPath}/lastSuccessfulBuild/buildNumber'
+                            """,
                             returnStdout: true
                         ).trim()
 
@@ -54,15 +26,16 @@ pipeline {
                         // Fetch build details (parameters and environment variables)
                         def buildInfoJson = sh(
                             script: """
-                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
-                            '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions%5Bparameters%5B*%5D%5D'""",
+                            curl -s --user ${JENKINS_USER}:${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions%5Bparameters%5B*%5D%5D'
+                            """,
                             returnStdout: true
                         ).trim()
 
                         // Check if curl returned an error code
                         def buildInfoStatus = sh(
                             script: """
-                            curl -s -o /dev/null -w "%{http_code}" --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            curl -s -o /dev/null -w "%{http_code}" --user ${JENKINS_USER}:${JENKINS_TOKEN} \
                             '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions%5Bparameters%5B*%5D%5D'
                             """,
                             returnStdout: true
@@ -108,8 +81,9 @@ pipeline {
                         // Fetch Jenkins crumb for CSRF protection
                         def crumbResponse = sh(
                             script: """
-                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
-                            \${jenkinsUrl}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)""",
+                            curl -s --user ${JENKINS_USER}:${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
+                            """,
                             returnStdout: true
                         ).trim()
 
@@ -120,9 +94,10 @@ pipeline {
                         def triggerUrl = "${jenkinsUrl}/${jobPath}/buildWithParameters?${paramString}"
                         def triggerResponse = sh(
                             script: """
-                            curl -s -X POST --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            curl -s -X POST --user ${JENKINS_USER}:${JENKINS_TOKEN} \
                             -H "${crumbHeader}:${crumbValue}" \
-                            ${triggerUrl}""",
+                            '${triggerUrl}'
+                            """,
                             returnStdout: true
                         ).trim()
 
@@ -132,4 +107,4 @@ pipeline {
             }
         }
     }
-}                                                                                                                                                            
+}
