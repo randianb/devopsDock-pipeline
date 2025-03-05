df92b31e392cfca85617e057f1c09fe14d4572e3
Anouar Harrou <95416938+anouarharrou@users.noreply.github.com>
2025-03-05 01:28:29 +0000

    Update jenkinsJob.groovy

--- jenkinsJob.groovy
+++ jenkinsJob.groovy
@@ -1,7 +1,6 @@
 def jenkinsUrl = "https://cdp-jenkins-paas-xsf.fr.world.socgen"
 def jobPath = "job/DJD/job/CD-Deploy/job/openr-pipeline-int"
 
-
 pipeline {
     agent any
 
@@ -26,7 +25,7 @@ pipeline {
                         // Fetch build details (parameters and environment variables)
                         def buildInfoJson = sh(
                             script: """
-                            curl -v -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
                             '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions%5Bparameters%5B*%5D%5D'""",
                             returnStdout: true
                         ).trim()
@@ -67,7 +66,11 @@ pipeline {
 
                         // Construct parameters string for the new build
                         def paramString = parameters.collect { 
-                            "${it.name}=${it.value}"
+                            if (it.name == 'REGION') {
+                                return "${it.name}=${URLEncoder.encode(it.value, 'UTF-8')}" // URL encode REGION value
+                            } else {
+                                return "${it.name}=${it.value}"
+                            }
                         }.join('&')
 
                         echo "Parameters for New Build: ${paramString}"
@@ -100,29 +103,3 @@ pipeline {
         }
     }
 }
-
-=================================
-    i got this error :
-{ [1398 bytes data]
-* Connection #0 to host cdp-jenkins-paas-xsf.fr.world.socgen left intact
-[Pipeline] sh
-+ curl -s -o /dev/null -w '%{http_code}' --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/openr-pipeline-int/810/api/json?tree=actions%5Bparameters%5B*%5D%5D'
-[Pipeline] echo
-Curl Status: 200
-[Pipeline] echo
-Build Info JSON: {"_class":"org.jenkinsci.plugins.workflow.job.WorkflowRun","actions":[{"_class":"hudson.model.ParametersAction","parameters":[{"_class":"hudson.model.BooleanParameterValue","name":"LATEST_IMAGE","value":false},{"_class":"hudson.model.BooleanParameterValue","name":"INFRA","value":false},{"_class":"hudson.model.BooleanParameterValue","name":"DEPLOY_FRONTEND","value":false},{"_class":"hudson.model.BooleanParameterValue","name":"DEPLOY_BACKEND","value":false},{"_class":"hudson.model.StringParameterValue","name":"FRONTEND_VERSION","value":""},{"_class":"hudson.model.StringParameterValue","name":"BACKEND_VERSION","value":""},{"_class":"hudson.model.StringParameterValue","name":"VERSION_TYPE","value":"snapshots"},{"_class":"hudson.model.StringParameterValue","name":"REGION","value":"'[\"paris\"]'"}]},{"_class":"hudson.model.CauseAction"},{"_class":"jenkins.metrics.impl.TimeInQueueAction"},{},{"_class":"org.jenkinsci.plugins.workflow.libs.LibrariesAction"},{},{"_class":"hudson.plugins.git.util.BuildData"},{},{"_class":"org.jenkinsci.plugins.workflow.cps.EnvActionImpl"},{},{"_class":"hudson.plugins.git.util.BuildData"},{},{},{},{},{"_class":"org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction"},{"_class":"org.jenkinsci.plugins.pipeline.modeldefinition.actions.RestartDeclarativePipelineAction"},{},{"_class":"org.jenkinsci.plugins.workflow.job.views.FlowGraphAction"},{},{},{}]}
-[Pipeline] readJSON
-[Pipeline] echo
-Parameters for New Build: LATEST_IMAGE=false&INFRA=false&DEPLOY_FRONTEND=false&DEPLOY_BACKEND=false&FRONTEND_VERSION=&BACKEND_VERSION=&VERSION_TYPE=snapshots&REGION='["paris"]'
-[Pipeline] sh
-/var/jenkins_home/jobs/DJD/jobs/Build-Archived/jobs/JenkinsCron/workspace@tmp/durable-5d9c81d5/script.sh.copy: line 2: syntax error near unexpected token `('
-[Pipeline] }
-[Pipeline] // script
-[Pipeline] }
-[Pipeline] // withCredentials
-[Pipeline] }
-[Pipeline] // stage
-[Pipeline] }
-[Pipeline] // node
-[Pipeline] End of Pipeline
-ERROR: script returned exit code 2
