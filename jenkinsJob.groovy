5f5156955a031a5bc5ce4983a6782e796d7c933c
Anouar Harrou <95416938+anouarharrou@users.noreply.github.com>
2025-03-05 00:59:55 +0000

    Update jenkinsJob.groovy

--- jenkinsJob.groovy
+++ jenkinsJob.groovy
@@ -1,6 +1,9 @@
-i found that the error is on this "                        def buildInfoJson = sh(
-                            script: """
-                            curl -v -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
-                            '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions[parameters[*]]'""",
-                            returnStdout: true
-                        ).trim()" the result is this "https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/openr-pipeline-int/810/api/json?tree=actions[parameters[*" wich is not valid it should return this "https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/openr-pipeline-int/810/api/json?tree=actions[parameters[*]" instead 
+def buildInfoJson = sh(
+    script: """
+    curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+    "${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions[parameters[*]]"
+    """,
+    returnStdout: true
+).trim()
+
+echo "Requesting URL: ${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions[parameters[*]]"
