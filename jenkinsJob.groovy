36072488443becb465d09a1fe2f90a4571bc8ed4
Anouar Harrou <95416938+anouarharrou@users.noreply.github.com>
2025-03-05 11:32:08 +0000

    Update jenkinsJob.groovy

--- jenkinsJob.groovy
+++ jenkinsJob.groovy
@@ -1,5 +1,34 @@
+this's the error i got : 
++ curl -s --user '****:****' https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/lastSuccessfulBuild/buildNumber
+[Pipeline] echo
+Latest Successful Build Number: 32
+[Pipeline] sh
++ curl -s --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/32/api/json?tree=actions%5Bparameters%5B*%5D%5D'
+[Pipeline] sh
++ curl -s -o /dev/null -w '%{http_code}' --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/Reboot/32/api/json?tree=actions%5Bparameters%5B*%5D%5D'
+[Pipeline] echo
+Curl Status: 200
+[Pipeline] echo
+Build Info JSON: {"_class":"org.jenkinsci.plugins.workflow.job.WorkflowRun","actions":[{"_class":"hudson.model.ParametersAction","parameters":[{"_class":"hudson.model.StringParameterValue","name":"SERVICE","value":"unibank-branch-onboarding"},{"_class":"hudson.model.StringParameterValue","name":"ENV","value":"uat"},{"_class":"hudson.model.BooleanParameterValue","name":"REBOOT","value":true}]},{"_class":"hudson.model.CauseAction"},{"_class":"jenkins.metrics.impl.TimeInQueueAction"},{},{"_class":"org.jenkinsci.plugins.workflow.libs.LibrariesAction"},{},{"_class":"hudson.plugins.git.util.BuildData"},{},{"_class":"org.jenkinsci.plugins.workflow.cps.EnvActionImpl"},{},{"_class":"hudson.plugins.git.util.BuildData"},{"_class":"org.jenkinsci.plugins.workflow.cps.view.InterpolatedSecretsAction"},{},{},{},{},{"_class":"org.jenkinsci.plugins.displayurlapi.actions.RunDisplayAction"},{"_class":"org.jenkinsci.plugins.pipeline.modeldefinition.actions.RestartDeclarativePipelineAction"},{},{"_class":"org.jenkinsci.plugins.workflow.job.views.FlowGraphAction"},{},{},{}]}
+[Pipeline] readJSON
+[Pipeline] echo
+Parameters for New Build: SERVICE=unibank-branch-onboarding&ENV=uat&REBOOT=true
+[Pipeline] sh
+/var/jenkins_home/jobs/DJD/jobs/Build-Archived/jobs/JenkinsCron/workspace@tmp/durable-0b8f8032/script.sh.copy: line 2: syntax error near unexpected token `('
+[Pipeline] }
+[Pipeline] // script
+[Pipeline] }
+[Pipeline] // withCredentials
+[Pipeline] }
+[Pipeline] // stage
+[Pipeline] }
+[Pipeline] // node
+[Pipeline] End of Pipeline
+ERROR: script returned exit code 2
+                                                                                                                                                            
+here's the pipeline : 
 def jenkinsUrl = "https://cdp-jenkins-paas-xsf.fr.world.socgen"
-def jobPath = "job/DJD/job/CD-Deploy/job/openr-pipeline-int"
+def jobPath = "job/DJD/job/CD-Deploy/job/Reboot"
 
 pipeline {
     agent any
@@ -103,4 +132,4 @@ pipeline {
             }
         }
     }
-}
+}                                                                                                                                                            
