2adfefff34069299b9013d711b06fae60871e229
Anouar Harrou <95416938+anouarharrou@users.noreply.github.com>
2025-03-05 00:42:27 +0000

    Update jenkinsJob.groovy

--- jenkinsJob.groovy
+++ jenkinsJob.groovy
@@ -1,58 +1,93 @@
-curl -s --user '****:****' https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/openr-pipeline-int/lastSuccessfulBuild/buildNumber
-[Pipeline] echo
-Latest Successful Build Number: 810
-[Pipeline] sh
-+ curl -v -s --user '****:****' 'https://cdp-jenkins-paas-xsf.fr.world.socgen/job/DJD/job/CD-Deploy/job/openr-pipeline-int/810/api/json?tree=actions[parameters[*]]'
-[Pipeline] }
-[Pipeline] // script
-[Pipeline] }
-[Pipeline] // withCredentials
-[Pipeline] }
-[Pipeline] // stage
-[Pipeline] }
-[Pipeline] // node
-[Pipeline] End of Pipeline
-Also:   hudson.remoting.ProxyException: org.jenkinsci.plugins.workflow.actions.ErrorAction$ErrorId: 7ce73e6a-677d-4446-a74f-1bbd283c4866
-hudson.remoting.ProxyException: groovy.lang.MissingMethodException: No signature of method: java.lang.Integer.trim() is applicable for argument types: () values: []
-Possible solutions: wait(), grep(), wait(long), print(java.io.PrintWriter), print(java.lang.Object), times(groovy.lang.Closure)
-	at org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SandboxInterceptor.onMethodCall(SandboxInterceptor.java:159)
-	at org.kohsuke.groovy.sandbox.impl.Checker$1.call(Checker.java:178)
-	at org.kohsuke.groovy.sandbox.impl.Checker.checkedCall(Checker.java:182)
-	at com.cloudbees.groovy.cps.sandbox.SandboxInvoker.methodCall(SandboxInvoker.java:17)
-	at org.jenkinsci.plugins.workflow.cps.LoggingInvoker.methodCall(LoggingInvoker.java:117)
-	at WorkflowScript.run(WorkflowScript:26)
-	at ___cps.transform___(Native Method)
-	at com.cloudbees.groovy.cps.impl.ContinuationGroup.methodCall(ContinuationGroup.java:90)
-	at com.cloudbees.groovy.cps.impl.FunctionCallBlock$ContinuationImpl.dispatchOrArg(FunctionCallBlock.java:116)
-	at com.cloudbees.groovy.cps.impl.FunctionCallBlock$ContinuationImpl.fixName(FunctionCallBlock.java:80)
-	at jdk.internal.reflect.GeneratedMethodAccessor419.invoke(Unknown Source)
-	at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-	at java.base/java.lang.reflect.Method.invoke(Method.java:568)
-	at com.cloudbees.groovy.cps.impl.ContinuationPtr$ContinuationImpl.receive(ContinuationPtr.java:72)
-	at com.cloudbees.groovy.cps.impl.ConstantBlock.eval(ConstantBlock.java:21)
-	at com.cloudbees.groovy.cps.Next.step(Next.java:83)
-	at com.cloudbees.groovy.cps.Continuable.run0(Continuable.java:147)
-	at org.jenkinsci.plugins.workflow.cps.SandboxContinuable.access$001(SandboxContinuable.java:17)
-	at org.jenkinsci.plugins.workflow.cps.SandboxContinuable.run0(SandboxContinuable.java:49)
-	at org.jenkinsci.plugins.workflow.cps.CpsThread.runNextChunk(CpsThread.java:180)
-	at org.jenkinsci.plugins.workflow.cps.CpsThreadGroup.run(CpsThreadGroup.java:423)
-	at org.jenkinsci.plugins.workflow.cps.CpsThreadGroup$2.call(CpsThreadGroup.java:331)
-	at org.jenkinsci.plugins.workflow.cps.CpsThreadGroup$2.call(CpsThreadGroup.java:295)
-	at org.jenkinsci.plugins.workflow.cps.CpsVmExecutorService.lambda$wrap$4(CpsVmExecutorService.java:140)
-	at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-	at hudson.remoting.SingleLaneExecutorService$1.run(SingleLaneExecutorService.java:139)
-	at jenkins.util.ContextResettingExecutorService$1.run(ContextResettingExecutorService.java:28)
-	at jenkins.security.ImpersonatingExecutorService$1.run(ImpersonatingExecutorService.java:68)
-	at jenkins.util.ErrorLoggingExecutorService.lambda$wrap$0(ErrorLoggingExecutorService.java:51)
-	at java.base/java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:539)
-	at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:264)
-	at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1136)
-	at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:635)
-	at org.jenkinsci.plugins.workflow.cps.CpsVmExecutorService$1.call(CpsVmExecutorService.java:53)
-	at org.jenkinsci.plugins.workflow.cps.CpsVmExecutorService$1.call(CpsVmExecutorService.java:50)
-	at org.codehaus.groovy.runtime.GroovyCategorySupport$ThreadCategoryInfo.use(GroovyCategorySupport.java:136)
-	at org.codehaus.groovy.runtime.GroovyCategorySupport.use(GroovyCategorySupport.java:275)
-	at org.jenkinsci.plugins.workflow.cps.CpsVmExecutorService.lambda$categoryThreadFactory$0(CpsVmExecutorService.java:50)
-	at java.base/java.lang.Thread.run(Thread.java:833)
-/var/jenkins_home/jobs/DJD/jobs/Build-Archived/workspace/JenkinsCron@tmp/jfrog/26/.jfrog deleted
-Finished: FAILURE
+def jenkinsUrl = "https://cdp-jenkins-paas-xsf.fr.world.socgen"
+def jobPath = "job/DJD/job/CD-Deploy/job/openr-pipeline-int"
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Get Last Success Build') {
+            steps {
+                withCredentials([
+                    string(credentialsId: 'jenkins-user', variable: 'JENKINS_USER'),
+                    string(credentialsId: 'jenkins-token', variable: 'JENKINS_TOKEN')
+                ]) {
+                    script {
+                        // Get the last successful build number
+                        def buildNumber = sh(
+                            script: """
+                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/${jobPath}/lastSuccessfulBuild/buildNumber'""",
+                            returnStdout: true
+                        ).trim()
+
+                        echo "Latest Successful Build Number: ${buildNumber}"
+
+                        // Fetch build details (parameters and environment variables)
+                        def buildInfoJson = sh(
+                            script: """
+                            curl -v -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/${jobPath}/${buildNumber}/api/json?tree=actions[parameters[*]]'
+                            """,
+                            returnStdout: true,
+                            returnStatus: true
+                        ).trim()
+
+                        // Check if curl returned an error code
+                        if (buildInfoJson == '3' || buildInfoJson.isEmpty()) {
+                            echo "Error: Received invalid response or error code 3"
+                            error "Failed to fetch valid build info"
+                        }
+
+                        echo "Build Info JSON: ${buildInfoJson}"
+
+                        // Attempt to parse the JSON and extract parameters
+                        def buildInfo = [:]
+                        try {
+                            buildInfo = readJSON text: buildInfoJson
+                        } catch (Exception e) {
+                            echo "Error parsing JSON: ${e.message}"
+                            error "Failed to parse JSON response"
+                        }
+
+                        def parameters = []
+                        buildInfo.actions.each { action ->
+                            if (action.parameters) {
+                                parameters.addAll(action.parameters)
+                            }
+                        }
+
+                        // Construct parameters string for the new build
+                        def paramString = parameters.collect { 
+                            "${it.name}=${it.value}"
+                        }.join('&')
+
+                        echo "Parameters for New Build: ${paramString}"
+
+                        // Fetch Jenkins crumb for CSRF protection
+                        def crumbResponse = sh(
+                            script: """
+                            curl -s --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            '${jenkinsUrl}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'""",
+                            returnStdout: true
+                        ).trim()
+
+                        def crumbHeader = crumbResponse.split(":")[0]
+                        def crumbValue = crumbResponse.split(":")[1]
+
+                        // Trigger a new build with the same parameters
+                        def triggerUrl = "${jenkinsUrl}/${jobPath}/buildWithParameters?${paramString}"
+                        def triggerResponse = sh(
+                            script: """
+                            curl -s -X POST --user \${JENKINS_USER}:\${JENKINS_TOKEN} \
+                            -H "${crumbHeader}: ${crumbValue}" \
+                            '${triggerUrl}'""",
+                            returnStdout: true
+                        ).trim()
+
+                        echo "Build Trigger Response: ${triggerResponse}"
+                    }
+                }
+            }
+        }
+    }
+}
